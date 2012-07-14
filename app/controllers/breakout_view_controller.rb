09cbb1c2602c9a0c2a67ae62ecc7f29834ea139f
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-07-14 13:45:13 +0530

    Make the bar movable.

--- app/controllers/breakout_view_controller.rb
+++ app/controllers/breakout_view_controller.rb
@@ -6,6 +6,25 @@ class BreakoutViewController < UIViewController
     height = 15
     @bar = UIView.alloc.initWithFrame([[(view.frame.size.width - width) / 2, view.frame.size.height - height], [width, height]])
     @bar.backgroundColor = UIColor.blueColor
+
+    @barPanGestureRecognizer = UIPanGestureRecognizer.alloc.initWithTarget(self, action:"barPanGesture:")
+    @bar.addGestureRecognizer(@barPanGestureRecognizer)
+
     view.addSubview(@bar)
+  end
+
+  def barPanGesture(sender)
+    point = sender.translationInView(@bar)
+    sender.setTranslation([0, 0], inView:@bar)
+    frame = @bar.frame
+    frame.origin.x += point.x
+
+    if frame.origin.x < 0
+      frame.origin.x = 0
+    elsif frame.origin.x > view.frame.size.width - frame.size.width
+      frame.origin.x = view.frame.size.width - frame.size.width
+    end
+
+    @bar.frame = frame
   end
 end
