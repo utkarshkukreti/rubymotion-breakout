a9718d50bbdc5b5523dfbf5267ad6eac18ddcb95
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2012-07-14 12:04:43 +0530

    Blue colored bar at the bottom.

--- app/controllers/breakout_view_controller.rb
+++ app/controllers/breakout_view_controller.rb
@@ -1,5 +1,11 @@
 class BreakoutViewController < UIViewController
   def viewDidLoad
     view.backgroundColor = UIColor.whiteColor
+
+    width = 100
+    height = 15
+    @bar = UIView.alloc.initWithFrame([[(view.frame.size.width - width) / 2, view.frame.size.height - height], [width, height]])
+    @bar.backgroundColor = UIColor.blueColor
+    view.addSubview(@bar)
   end
 end
